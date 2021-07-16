6648ee6b566298aac42dcbaba27f449e3eb16533
erikasnts22 <sntserika22@gmail.com>
2021-07-16 18:59:45 +0800

    Catching up with the activities

---
+++ disemvowel_trolls.rb
@@ -0,0 +1,16 @@
+=begin
+DISEMVOWEL TROLLS
+
+Trolls are attacking your comment section!
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+=end
+
+def disemvowel(string)
+    result = string.gsub(/[aeiou]/i, '')
+    return result
+end
+  
+  #tests
+  puts disemvowel("This website is for losers LOL!") 
+  puts disemvowel("I love Ruby on Rails")
