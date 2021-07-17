3f5ea0f39cb2c3f46cef0fefe31f4d5570789d30
erikasnts22 <sntserika22@gmail.com>
2021-07-17 13:54:34 +0800

    catching up with activities

---
+++ format_string_names.rb
@@ -0,0 +1,13 @@
+=begin
+Coding Exercise: Format a string of names like 'Bart, Lisa &
+Maggie'.
+
+FORMAT A STRING OF NAMES LIKE 'BART, LISA & MAGGIE'.
+
+Given: an array containing hashes of names
+Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated
+by an ampersand.
+=end
+def format_string_names(names)
+    
+end
