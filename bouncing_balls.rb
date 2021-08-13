57b43567909da45662ca39c2583757ed7eca58c3
erikasnts22 <sntserika22@gmail.com>
2021-08-13 12:05:44 +0800

    bouncing balls activity

--- bouncing_balls.rb
+++ bouncing_balls.rb
@@ -12,6 +12,6 @@ cond = height > 0 && (bounce > 0 && bounce < 1) && window < height
       return -1
     end
 end
-    # tests
-    puts bouncingBalls(height = 3, bounce = 0.66, window = 1.5)
-    puts bouncingBalls(height = 3, bounce = 1, window = 1.5)
+
+puts bouncingBalls(height = 3, bounce = 0.66, window = 1.5)
+puts bouncingBalls(height = 3, bounce = 1, window = 1.5)
